88643e527fbceaeeb40e5f2f62ad87eca8bc3242
esotericpig <esotericpig@users.noreply.github.com>
2020-04-26 21:40:02 +0800

    Add base working idea for jsonify

--- lib/unclekryon/jsoner.rb
+++ lib/unclekryon/jsoner.rb
@@ -34,24 +34,67 @@ require 'yaml'
 require 'unclekryon/iso'
 require 'unclekryon/util'
 
+require 'unclekryon/data/artist_data'
+require 'unclekryon/data/artist_data_data'
+
 module UncleKryon
   class Jsoner
     def jsonify_all(pretty=false)
       json = {}
       
-      jsonify_iso(json)
+      #jsonify_iso(json)
+      jsonify_artists(json)
       
       return pretty ? JSON.pretty_generate(json) : json.to_json()
     end
     
+    def jsonify_artists(json)
+      json['aum'] = {}
+      json['scroll'] = {}
+      json['vision'] = {}
+      json['pic'] = {}
+      
+      kryon = to_hash(ArtistData.load_file(File.join('hax','kryon.yaml')))
+      
+      kryon['release'] = {}
+      kryon['album'] = {}
+      
+      jsonify_artist_data(json,kryon,File.join('hax','kryon_aums_2002-2005.yaml'))
+      
+      json['artist'] = {
+        kryon['id'] => kryon
+      }
+    end
+    
+    def jsonify_artist_data(json,artist,file)
+      data = ArtistDataData.load_file(file)
+      
+      data.albums.each() do |album_id,album|
+        album.aums.each() do |aum_id,aum|
+          json[ArtistDataData::AUMS_ID][aum_id] = to_hash(aum)
+        end
+        album.aums = album.aums.keys
+        
+        album.pics.each() do |pic_id,pic|
+          json[ArtistDataData::PICS_ID][pic_id] = to_hash(pic)
+        end
+        album.pics = album.pics.keys
+      end
+      
+      artist[ArtistDataData::ALBUMS_ID] = to_hash(data.albums)
+      
+      #attr_accessor :scrolls
+      #attr_accessor :visions
+    end
+    
     def jsonify_iso(json)
-      json[Iso::ID] = to_hash(Iso.iso)
-      json[Iso.can_provs_terrs.id] = to_hash(Iso.can_provs_terrs.values)
-      json[Iso.countries.id] = to_hash(Iso.countries.values)
-      json[Iso.languages.id] = to_hash(Iso.languages.values)
-      json[Iso.regions.id] = to_hash(Iso.regions.values)
-      json[Iso.subregions.id] = to_hash(Iso.subregions.values)
-      json[Iso.usa_states.id] = to_hash(Iso.usa_states.values)
+      json['iso'] = to_hash(Iso.iso)
+      json['can_proter'] = to_hash(Iso.can_provs_terrs.values)
+      json['country'] = to_hash(Iso.countries.values)
+      json['language'] = to_hash(Iso.languages.values)
+      json['region'] = to_hash(Iso.regions.values)
+      json['subregion'] = to_hash(Iso.subregions.values)
+      json['usa_state'] = to_hash(Iso.usa_states.values)
     end
     
     def to_hash(obj)
